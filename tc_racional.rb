89ec05d90be7c6102908c66d6eba8b32da0d73ea
MORALES PEREZ <alu4171@exthost2.(none)>
2013-10-18 22:29:42 +0100

    practica terminada

--- tc_racional.rb
+++ tc_racional.rb
@@ -4,5 +4,30 @@ require "./racional.rb"
 require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
+        def setup
+                @a = Fraccion.new(14,3)
+                @b = Fraccion.new(10,3)
+        end
+
+        def test_suma
+                assert_equal(Fraccion.new(100,50), @a.suma(@b))
+        end
+
+        def test_resta
+                assert_equal(Fraccion.new(4,3), @a.resta(@b))
+        end
+
+        def test_producto
+                assert_equal(Fraccion.new(8,10), @a.producto(@b))
+        end
+
+        def test_division
+                assert_equal(Fraccion.new(7,5), @a.division(@b))
+        end
+
+        def test_typecheck
+                assert_raise ( RuntimeError ) { Fraccion.new('a','a') }
+                assert_raise ( RuntimeError ) { Fraccion.new(13.2,3.5)}
+        end
 
 end
